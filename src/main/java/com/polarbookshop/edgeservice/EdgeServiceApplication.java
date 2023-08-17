f683c91209f387ad932dc54cda3302399e8cb5f1
cometbid-project <20684020+cometbid-project@users.noreply.github.com>
2023-08-17 16:38:10 -0400

    first commit
    Initial commit

---
+++ src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,18 @@
+package com.polarbookshop.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Gbenga
+ *
+ */
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.polarbookshop.edgeservice.config;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gbenga
+ *
+ */
+@Configuration
+public class RateLimiterConfig {
+
+	@Bean
+	public KeyResolver keyResolver() {
+		return exchange -> Mono.just("anonymous");
+	}
+	
+}
